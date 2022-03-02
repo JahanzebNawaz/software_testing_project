3eaa9073edcfcd801ac195631f21676c30dce185
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2022-03-02 17:11:37 -0500

    chrome settings update

--- PROJECT/tests/base_test.py
+++ PROJECT/tests/base_test.py
@@ -14,6 +14,7 @@ class BaseTestCase(unittest.TestCase):
         self.website_url = WEBSITE_URL
         chrome_options = Options()
         chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         self.service = Service(ChromeDriverManager().install())
