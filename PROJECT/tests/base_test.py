bffe19fdd320328bb1a1c0ba561595eaf38c1306
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2022-03-02 14:42:31 -0500

    chrome options

--- PROJECT/tests/base_test.py
+++ PROJECT/tests/base_test.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from PROJECT.constant import WEBSITE_URL, LOGOUT_URL
 import time
 import pytest
@@ -10,8 +11,12 @@ class BaseTestCase(unittest.TestCase):
 
     def setUp(self):
         self.website_url = WEBSITE_URL
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
         self.browser = webdriver.Chrome(
-            executable_path=ChromeDriverManager().install()
+            executable_path=ChromeDriverManager().install(), options=chrome_options
         )
         self.browser.maximize_window()
         self.addCleanup(self.browser.quit)
