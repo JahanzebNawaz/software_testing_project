e57f50d0f6f0830b1fea02da9a29242dbec09bae
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2022-03-03 08:50:16 -0500

    chrome fix

--- PROJECT/tests/base_test.py
+++ PROJECT/tests/base_test.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from PROJECT.constant import WEBSITE_URL, LOGOUT_URL
 import time
-import pytest
+import os
 
 
 class BaseTestCase(unittest.TestCase):
@@ -13,13 +13,19 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.website_url = WEBSITE_URL
         chrome_options = Options()
+        # chrome_options.binary_location = '/usr/bin/google-chrome'
         chrome_options.add_argument("--disable-extensions")
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         self.service = Service(ChromeDriverManager().install())
         self.browser = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), options=chrome_options
+            service=self.service,
+            options=chrome_options
         )
         self.browser.maximize_window()
         self.addCleanup(self.browser.quit)
